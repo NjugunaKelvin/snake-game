5c3077647937b922db32403441813518de75af6f
NjugunaKelvin <njugunak349@gmail.com>
2025-01-23 16:01:15 +0300

    added the food submodule

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+
 # turn off tracer/ animation
 screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,23 +27,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
     
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
